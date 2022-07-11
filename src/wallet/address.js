766c42b944bafad0107eec8ef4b293e4ad51d1a4
smon4k <smon4k@gmail.com>
2022-07-11 12:48:52 +0800

    no message

--- src/wallet/address.js
+++ src/wallet/address.js
@@ -45,13 +45,13 @@ export default {
     "nftMarket":"0x25760bF19CC577ef012AB0c2dA033C8dFbcF3419",
     "fragmentToken":"0x15b0eE096Ea786160390d5cD7816801E5fed871d",
     "LPToken":"0x873f6f45CBD70fEcD342F5BdaF105dD41268D44F", //lp deployed
-    "singlePool":"0x1136Ef1E8a3b1991232cD45D2BEA873e5D8AF202", //singlePool deployed
-    "nftEarnErc20Pool":"0xF5D753F937152e67e557F913f5cE5514f312042a", //nftEarnErc20Pool deployed
+    "singlePool":"0x9E53637957C2b40D4e6494936D5B5b3c2AD535CA", //singlePool deployed
+    "nftEarnErc20Pool":"0xEA18CF67Ff4635b5EbB5D976D939ba4721677E63", //nftEarnErc20Pool deployed
     "MintPrice":"0.3",
 
-    "IDORewardToken": "0x5099DF0bA065440757817eC9B84db779aF5f0160", //reward token
+    "IDORewardToken": "0xc3C57DAc16412900BA320d65120389E5F9df8Eb3", //reward token
     "IDOUsToken": "0x55d398326f99059ff775485246999027b3197955", //usToken deployed
-    "IDOToken": "0x92c77c482DD1DFA32f9603ae67F4A2521FCF76F1", //IDO deployed
+    "IDOToken": "0xe5996fCF2f4D7acc63aa5da026dA9173015041E7", //IDO deployed
     "IDOInvitePool": "0x858e6F03C9c4D21AcA313630E58cB57F2cD2B1B2", //invitePool deployed
     // "IDODsgNftDeployed": "0x79fEa54444BC0e4CC03fe2C32060434f5dAc2942", //直推合约地址
     nftImgSrc,
